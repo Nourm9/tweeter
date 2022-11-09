f57e650d1c0eb0cecd1167ac5ce6c23c11eae201
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-11-09 00:42:11 +0000

    adding character counter, tweet section and styles

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // let value = 0;
-  $("#count").keyup(function(event) {
-    
-    $("#countNum").text(140-$(this).val().length);
-    
-
- });
-  
+  $("#count").keyup(function (event) {
+    $("#countNum").text(140 - $(this).val().length);
+  });
 });
